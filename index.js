76390671fb0b957a82bbff9a446b34ff185afbda
Michał Budzyński <michal@virtualdesign.pl>
2016-04-10 17:17:00 +0100

    :cloud: custom letters

--- index.js
+++ index.js
@@ -7,14 +7,25 @@ for (i = 0; i < defaultString.length; i++ ) {
   letters[defaultString[i]] = i;
 }
 
-console.log(letters);
-
 var wildstrings = function(string) {
-  var output = 'S=String.fromCharCode;a=\'siem\'.link();';
+  var missingLetters = '';
+  var output = 'S=String.fromCharCode;a=\'';
+  var output2 = '\'.link();';
   for (var i = 0; i<string.length; i++) {
-    if (letters[string[i].toLowerCase()] !== undefined) {
-      output += 'a[' + letters[string[i].toLowerCase()] + ']+';
+    if (letters[string[i].toLowerCase()] === undefined) {
+      missingLetters += string[i].toLowerCase();
      }
+  }
+  //output += 'a[' + letters[string[i].toLowerCase()] + ']+';
+
+  defaultString = missingLetters.link();
+  for (i = 0; i < defaultString.length; i++ ) {
+    letters[defaultString[i]] = i;
+  }
+  output += missingLetters;
+  output += output2;
+  for (i = 0; i<string.length; i++) {
+    output += 'a[' + letters[string[i].toLowerCase()] + ']+';
   }
 
   console.log(output);
